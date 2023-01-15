0b0f204ff5d8ae1f0c51737e8d809d6d1e95f4a7
xelox <alexg@cornerstoneremovals.co.uk>
2023-01-15 21:24:45 +0000

    first commit

---
+++ frontend/src/components/views/infoView.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let rarity = 5;
+    let name = 'Emblem of Severed Fate'
+</script>
+<style>
+    main{
+        background-color: wheat;
+        width: 300px;
+        min-height: 300px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+    
+</style>
+
+<main>
+    <div class="{`title title_${rarity}`}">
+        {name}
+    </div>
+</main>
